0435ed8a2a076e789cc697160dabb9949953fc10
Chinmay Chougule <84652075+Chinmay1292@users.noreply.github.com>
2023-11-30 00:33:28 +0530

    Update in Work exp

--- portfolio/src/components/testimonials/Testimonials.jsx
+++ portfolio/src/components/testimonials/Testimonials.jsx
@@ -15,29 +15,28 @@ const Testimonials = () => {
   const testimonials = [
     {
       id: 1,
-      link: 'https://www.linkedin.com/posts/chinmay-chougule-478909213_connections-internship-cybersecurity-activity-7040353849861103616-inKv?utm_source=share&utm_medium=member_desktop',
-      name: 'Hacker Bro Technologies',
-      role: 'Cyber Security Analyst Intern (03-23 - Ongoing)',
-      test: 'As a Cyber Security Analyst Intern, my current role is to learn, analyze, monitor, perform vulnerability/risk assessments, and report on the activities that have been assigned and requested for me. Hacker Bro Technologies is assisting me in gaining practical understanding of how to identify and patch the issues discovered.',
+      name: 'Newfold Digital',
+      role: 'Upcoming Tech Operations Engineer (01-24 - Ongoing)',
+      test: 'Upcoming Tech Operations Engineer for Newfold Digital India.',
     },
     {
       id: 2,
+      link: 'https://www.linkedin.com/posts/chinmay-chougule-478909213_connections-internship-cybersecurity-activity-7040353849861103616-inKv?utm_source=share&utm_medium=member_desktop',
+      name: 'Hacker Bro Technologies - Internship',
+      role: 'Cyber Security Analyst Intern (03-23 - 06-23)',
+      test: 'As a Cyber Security Analyst Intern, my current role is to learn, analyze, monitor, perform vulnerability/risk assessments, and report on the activities that have been assigned and requested for me. Hacker Bro Technologies is assisting me in gaining practical understanding of how to identify and patch the issues discovered.',
+    },
+    {
+      id: 3,
       link: 'https://www.cloudskillsboost.google/public_profiles/c05cbefd-43fd-4f2a-be0d-7f8f81574350',
-      name: 'Google Cloud Ready Facilitator',
+      name: 'Google Cloud Ready Facilitator - Volunteering',
       role: 'Facilitator (04-22 - 07-22)',
       test: "Facilitator in Google Cloud Ready Facilitator 2022. Earned various skill badges in GCP and completed tracks. Helped a lot people around to achieve the same.",
     },
     {
-      id: 3,
-      link: 'https://www.credly.com/badges/45e2125b-34a2-4631-add2-e271524dad96',
-      name: 'Microsoft Certified: Azure Fundamentals',
-      role: 'Externship (06-22 - 07-22)',
-      test: 'Cleared the Microsoft Azure AZ-900 Certification. This certification gives the fundamental aspects and benefits of Microsoft’s Cloud Services. Learnt how subscriptions work, what support plans are available, the difference between Infrastructure as a Service, Platform as a Service and Software as a Service.',
-    },
-    {
       id: 4,
       link: 'https://drive.google.com/file/d/1jgu0Ks73MCugqjy4a_2grcW63I6WWxWz/view?usp=share_link',
-      name: 'Marathi Club of VIT, Bhopal',
+      name: 'Marathi Club of VIT, Bhopal - Volunteering',
       role: 'Design Lead (01-23 - 02-23)',
       test: "Worked as head of Design and Creatives Team of Marathi Club VIT, Bhopal and as a Core Organizer.",
     },
@@ -45,7 +44,7 @@ const Testimonials = () => {
   return (
     <section id="testmonials">
       <h5>How I work</h5>
-      <h2>Work Experience</h2>
+      <h2>Work Experience and Volunteering</h2>
       <Swiper 
         className="container testimonials__container"
         modules={[Pagination]}
